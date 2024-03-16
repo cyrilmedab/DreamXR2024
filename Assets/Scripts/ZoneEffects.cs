a0c7f85882de4df0725330a46e5b48bf0f6a1e31
Cyril Medabalimi <cyrildmed@gmail.com>
2024-03-16 00:50:06 -0700

    Destroys Notes on Collision when Avatar is present in the zone

--- Assets/Scripts/ZoneEffects.cs
+++ Assets/Scripts/ZoneEffects.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ZoneEffects : MonoBehaviour
 {
     private AudioSource _zoneSound;
+    private bool _playerInZone;
 
     private void Awake()
     {
@@ -24,7 +25,26 @@ public class ZoneEffects : MonoBehaviour
 
     private void OnTriggerEnter(Collider collider)
     {
-        PlayAllZoneEffects();
-        Debug.Log("Collision");
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            _playerInZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _playerInZone = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Note") && _playerInZone)
+        {
+            Destroy(other.gameObject);
+            PlayAllZoneEffects();
+        }
     }
 }
