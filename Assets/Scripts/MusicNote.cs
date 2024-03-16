b91810bbe1fea550dff038c5441f2fdb7cc365ac
Cyril Medabalimi <cyrildmed@gmail.com>
2024-03-16 00:16:36 -0700

    Added randomized Spawn Manager with variable spawn speed and location

--- Assets/Scripts/MusicNote.cs
+++ Assets/Scripts/MusicNote.cs
@@ -6,9 +6,12 @@ public class MusicNote : MonoBehaviour
 {
     public float noteSpeed = 2f;
 
+    [SerializeField] private float bottomBound = -10f;
+
     // Update is called once per frame
     void Update()
     {
         transform.Translate(-Vector3.forward * Time.deltaTime * noteSpeed);
+        if (transform.position.z < bottomBound) Destroy(gameObject);
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,17 +7,33 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject[] notePrefabs;
     [SerializeField] private Transform[] spawnPoints;
 
-    void Update()
+    [SerializeField] private float startDelay = 10f;
+
+    [SerializeField] private float spawnLowerBound = 0.5f;
+    [SerializeField] private float spawnHigherBound = 1.5f;
+
+
+    private void Start()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        StartCoroutine(SpawnRandomNote());
+    }
+
+    private IEnumerator SpawnRandomNote()
+    {
+        yield return new WaitForSeconds(startDelay);
+
+        while(true)
         {
             int noteIndex = Random.Range(0, notePrefabs.Length);
             int spawnIndex = Random.Range(0, spawnPoints.Length);
 
             Vector3 spawnPos = spawnPoints[spawnIndex].position;
 
-            Instantiate(notePrefabs[noteIndex], spawnPos, 
+            Instantiate(notePrefabs[noteIndex], spawnPos,
                 notePrefabs[noteIndex].transform.rotation);
+
+            float spawnInterval = Random.Range(spawnLowerBound, spawnHigherBound);
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 }
